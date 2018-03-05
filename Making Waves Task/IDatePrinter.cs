2635cee7382fd93a75d893f587c7f4dbb45502ec
Marcin Danak <danakmarcin@gmail.com>
2018-03-05 23:07:26 +0100

    Delete unused namespaces.

--- Making Waves Task/IDatePrinter.cs
+++ Making Waves Task/IDatePrinter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Making_Waves_Task
+﻿namespace Making_Waves_Task
 {
     public interface IDatePrinter
     {
--- Making Waves Task/Program.cs
+++ Making Waves Task/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Globalization;
 
 namespace Making_Waves_Task
 {
